6b3f5a22d846fdb49ab92407af4fea29db0d2d5f
VeitchM <matiasveitch@gmail.com>
2022-04-27 14:15:33 -0400

    Starting pidFile in client

--- src/dbusClient.js
+++ src/dbusClient.js
@@ -1,16 +1,23 @@
 const DBus = require('dbus');
 const { dbusServiceName, dbusObjectName, dbusInterfaceName } = require('../config/default');
 
+process.umask(0000)
 
-//
-const logPath = '/var/log/swlibrelog'
 const fs = require('fs');
+
+const pidFilePath = '/var/run/swlibreclient.pid'
+if (fs.existsSync(pidFilePath)){
+    process.exit(1)
+}
+else{
+    fs.writeFileSync(pidFilePath, process.pid)
+}
+
+const logPath = '/var/log/swlibrelog'
 const logFileStream = fs.createWriteStream(logPath, {flags:'a'});
-
-
+ 
 const port = 3000;
 const bus = DBus.getBus('session');
-
 
 
 bus.getInterface(dbusServiceName, dbusObjectName, dbusInterfaceName, function(err, iface) {
@@ -34,4 +41,5 @@ app.listen(port, () => {
 process.on('SIGTERM', function () {
     if (server === undefined) return
     server.close();
+    fs.rm(pidFilePath)
 });
